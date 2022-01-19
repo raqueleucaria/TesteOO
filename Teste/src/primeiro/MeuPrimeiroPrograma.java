dd453383dbbf2ade69152b5fe8480a8df8a368a6
raqueleucaria <raqueleucaria@gmail.com>
2022-01-19 18:18:18 -0300

    Testando o commit com token

--- Teste/src/primeiro/MeuPrimeiroPrograma.java
+++ Teste/src/primeiro/MeuPrimeiroPrograma.java
@@ -5,6 +5,7 @@ public class MeuPrimeiroPrograma {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Meu primeiro programa em java!");
+		System.out.print("Testando o commit!");
 	}
 
 }
